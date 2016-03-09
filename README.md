0b6032079d2739fd01ae6387549181b145df9066
Humberto Dias <hldtux@gmail.com>
2016-03-09 11:52:56 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# game-gizelda-libgdx
-RPG game like Zelda using libGDX
+# Games with libGDX
+
+RPG game like Zelda using libGDX Library
+
+
+## Prerequires
+
+1. Git 2.6+
+2. Gradle 2.11+
+3. Java 8+
+
+
+## How to Play
+
+Clone
+
+```
+git clone https://github.com/humbertodias/game-gizelda-libgdx.git
+```
+
+Inside the folder
+
+```
+cd game-gizelda-libgdx/src
+```
+
+Run
+
+```
+gradle desktop:run
+```
+
+
+### Output
+![Preview](doc/gizelda.gif)
+
+
+## References
+
+[libGDX HOME](https://libgdx.badlogicgames.com/)
+
+[libGDX with Maven](https://github.com/libgdx/libgdx/wiki/Maven-integration)
+
---
+++ src/html/src/br/polygon/game/gizelda/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package br.polygon.game.gizelda.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import br.polygon.game.gizelda.GizeldaGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GizeldaGame();
+        }
+}
